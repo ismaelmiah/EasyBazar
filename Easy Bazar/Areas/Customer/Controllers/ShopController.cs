83cb7179b3c78df4ccc013bb6b57a4af3eef91c7
ismail5g <ismail27.dec@gmail.com>
2020-04-28 16:18:06 +0600

    Shop Page Added through Shop Controllers

---
+++ Easy Bazar/Areas/Customer/Controllers/ShopController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DataSets.Interfaces;
+using DataSets.Utility;
+using DataSets.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Easy_Bazar.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShopController : Controller
+    {
+        private readonly IUnitOfWork _uow;
+        public ShopController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public IActionResult Index()
+        {
+            var model = new HomeVM
+            {
+                Products = _uow.Product.GetAll(includeProperties: "Category").ToList(),
+                Categories = _uow.Category.GetAll().ToList()
+            };
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                var shoppingCount = _uow.ShoppingCart.GetAll(a => a.ApplicationUserId == claim.Value).ToList().Count();
+
+                HttpContext.Session.SetInt32(ProjectConstant.shoppingCart, shoppingCount);
+            }
+            return View(model);
+        }
+    }
+}
